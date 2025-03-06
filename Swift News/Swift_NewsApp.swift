9002e35c18726168bb5cb56d3fd90e80426a70c6
Akash Bhattacharya <akashbhattacharyak1314@gmail.com>
2025-03-06 21:10:22 +0530

    Initial Commit

---
+++ Swift News/Swift_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_NewsApp.swift
+//  Swift News
+//
+//  Created by InxenseApp01 on 06/03/25.
+//
+
+import SwiftUI
+
+@main
+struct Swift_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Swift NewsTests/Swift_NewsTests.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_NewsTests.swift
+//  Swift NewsTests
+//
+//  Created by InxenseApp01 on 06/03/25.
+//
+
+import Testing
+@testable import Swift_News
+
+struct Swift_NewsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
